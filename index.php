f3bd88082fa04552c455c5f7c176216f258fb7f1
raraDw <146161438+raraDw@users.noreply.github.com>
2023-09-29 05:45:49 +0000

    update

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>makanan khas cilacap</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">KULINER</h2>
+            </div>
+
+             <div class="menu">
+                 <ul> 
+                     <li><a href="#">HOME</a></li>
+                     <li><a href="#">ABOUT</a></li>
+                     <li><a href="#">SERVICE</a></li>
+                     <li><a href="#">DESIGN</a></li>
+                     <li><a href="#">CONTACT</a></li>
+                 </ul>
+             </div>
+        </div>
+    </div>
+</body>
+</html>
